cf2439a8f1dd8b99292cb78e7fc06a40678e0f1e
Bridge <aa-BridgeJ@ucsfmedicalcenter.org>
2019-10-29 11:13:34 -0700

    drupal console and some messing around with my custom module

--- web/modules/custom/drupalup_simple_form/src/Form/SimpleForm.php
+++ web/modules/custom/drupalup_simple_form/src/Form/SimpleForm.php
@@ -2,6 +2,15 @@
 namespace Drupal\drupalup_simple_form\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+
+
+use \Drupal\node\Entity\Node;
+use \Drupal\user\Entity\User;
+use \Drupal\filter\Entity\FilterFormat;
+
+
+
+
 /**
  * Our simple form class.
  */
@@ -16,6 +25,21 @@ class SimpleForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+
+   /*
+   *  this just here to try things.
+   * sample patient node# 26046
+   */
+
+     #$node = Node::load(26046);
+     $node_storage = \Drupal::entityTypeManager()->getStorage('node');
+     $nid = 26046;
+     $node = $node_storage->load($nid);
+     #$users = User::loadMultiple(array(1, 2, 3));
+     #$format = FilterFormat::load(1);
+
+    #
+
     $form['number_1'] = [
       '#type' => 'textfield',
       '#title' => $this->t('First number'),
@@ -25,7 +49,10 @@ class SimpleForm extends FormBase {
       '#title' => $this->t('Second numbers'),
     ];
 
-    echo('just some text here');
+    #echo('just some text here');
+    echo('<pre>');
+    echo $node->field_first_name->value;
+    echo('</pre>');
 
     $form['submit'] = [
       '#type' => 'submit',
